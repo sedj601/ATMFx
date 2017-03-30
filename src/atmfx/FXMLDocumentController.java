e3a76a523dd76c5eab9de75c74fbeb3c0dfac935
sedj601 <>
2017-03-30 17:01:26 -0500

    Merge origin/Diver_latest into second
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/diverapp/Diver.java
    	src/diverapp/FXMLDocumentController.java
    	src/diverapp/Harpoon.java
    	src/diverapp/Squid.java

--- src/atmfx/FXMLDocumentController.java
+++ src/atmfx/FXMLDocumentController.java
@@ -6,11 +6,14 @@
 package atmfx;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
 
 /**
  *
@@ -19,20 +22,62 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable
 {
     
-    @FXML
-    private Label label;
+   
+    @FXML private GridPane gpKeyboard, gpNumberPad;
+    
+    
+    Map<String, Button> buttons = new HashMap<>();
+
     
     @FXML
-    private void handleButtonAction(ActionEvent event)
+    private void handleOnKeyReleased(KeyEvent event)
     {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        Button tempButton = buttons.get(event.getText());
+        System.out.println("Released: " + event.getText());
+        
+        if(tempButton != null)
+        {
+            tempButton.disarm();
+            tempButton.setStyle("-fx-background-color: yellow");          
+        }        
+        else if(event.getCode().equals(KeyCode.ENTER))
+        {
+            tempButton = buttons.get("enter");
+            tempButton.disarm();
+            tempButton.setStyle("-fx-background-color: yellow");
+        }
+    }
+    
+    @FXML
+    private void handleOnKeyPressed(KeyEvent event)
+    {
+        Button tempButton = buttons.get(event.getText());
+        System.out.println("Pressed: " + event.getText());
+        if(tempButton != null)
+        {
+            tempButton.arm();
+            tempButton.setStyle("-fx-background-color: blue");
+        }
+        
+        else if(event.getCode().equals(KeyCode.ENTER))
+        {
+            tempButton = buttons.get("enter");
+            tempButton.arm();
+            tempButton.setStyle("-fx-background-color: blue");
+        }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
         // TODO
-    }    
-    
+        for(Node tempNode : gpKeyboard.getChildren())
+        {
+            if(tempNode instanceof Button)
+            {
+                Button tempButton = (Button)tempNode;
+                buttons.put(tempButton.getText().toLowerCase(), tempButton);
+            }          
+        }
+    }       
 }
