0b354458d220338c96067287ff6e55867e764a83
sedj601 <>
2017-03-30 22:56:02 -0500

    Merge origin/Diver_latest into second
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/diverapp/Diver.java
    	src/diverapp/FXMLDocumentController.java
    	src/diverapp/Harpoon.java
    	src/diverapp/Squid.java

--- src/atmfx/FXMLDocumentController.java
+++ src/atmfx/FXMLDocumentController.java
@@ -5,12 +5,9 @@
  */
 package atmfx;
 
-import java.net.URL;
+import java.net.*;
 import java.util.*;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.*;
+import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.scene.input.*;
 import javafx.scene.layout.*;
@@ -19,65 +16,204 @@ import javafx.scene.layout.*;
  *
  * @author blj0011
  */
-public class FXMLDocumentController implements Initializable
-{
-    
-   
-    @FXML private GridPane gpKeyboard, gpNumberPad;
-    
-    
-    Map<String, Button> buttons = new HashMap<>();
+public class FXMLDocumentController implements Initializable {
 
-    
+    @FXML
+    private GridPane gpKeyboard, gpNumberPad;
+    @FXML
+    private AnchorPane apWelcomeScreen;
+    @FXML
+    private Button btnRightOne, btnRightTwo, btnRightThree, btnRightFour, btnLeftOne, btnLeftTwo, btnLeftThree, btnLeftFour;
+    //add check balance screen
+
+    Map<String, Button> buttons = new HashMap<>();
+    boolean isWelcomeScreenShowingi;
+    boolean isDepositScreenShowing;
+    boolean isWithdrawalScreenShowing;
+    boolean isSetupAccountScreenShowing;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        isWelcomeScreenShowingi = true;
+        isDepositScreenShowing = false;
+        isWithdrawalScreenShowing = false;
+        isSetupAccountScreenShowing = false;
+
+        btnLeftOne.setOnAction((event) ->
+        {
+            System.out.println("Loading Setup Account Screen");
+            showSetupAccountScreen();
+        });
+        btnLeftTwo.setOnAction(null);
+        btnLeftThree.setOnAction(null);
+        btnLeftFour.setOnAction(null);
+
+        btnRightOne.setOnAction((event) ->
+        {
+            System.out.println("Loading Withdrawal Screen");
+            showWithdrawalScreen();
+        });
+        btnRightTwo.setOnAction((event) ->
+        {
+            System.out.println("Loading Deposit Screen");
+            showDepositScreen();
+        });
+        btnRightThree.setOnAction(null);
+        btnRightFour.setOnAction(null);
+
+        gpKeyboard.getChildren().stream().filter((tempNode)
+                -> (tempNode instanceof Button)).map((
+                tempNode) -> (Button) tempNode).forEachOrdered((tempButton) ->
+        {
+            buttons.put(tempButton.getText().toLowerCase(), tempButton);
+        });
+    }
+
     @FXML
     private void handleOnKeyReleased(KeyEvent event)
     {
+
         Button tempButton = buttons.get(event.getText());
         System.out.println("Released: " + event.getText());
-        
-        if(tempButton != null)
+
+        if (tempButton != null)
         {
             tempButton.disarm();
-            tempButton.setStyle("-fx-background-color: yellow");          
-        }        
-        else if(event.getCode().equals(KeyCode.ENTER))
+            tempButton.setStyle("");
+        }
+        else if (event.getCode().equals(KeyCode.ENTER))
         {
             tempButton = buttons.get("enter");
             tempButton.disarm();
-            tempButton.setStyle("-fx-background-color: yellow");
+            tempButton.setStyle("");
+        }
+        else if (event.getCode().equals(KeyCode.BACK_SPACE))
+        {
+            tempButton = buttons.get("backspace");
+            tempButton.disarm();
+            tempButton.setStyle("");
         }
     }
-    
+
     @FXML
     private void handleOnKeyPressed(KeyEvent event)
     {
         Button tempButton = buttons.get(event.getText());
         System.out.println("Pressed: " + event.getText());
-        if(tempButton != null)
+        if (tempButton != null)
         {
             tempButton.arm();
             tempButton.setStyle("-fx-background-color: blue");
         }
-        
-        else if(event.getCode().equals(KeyCode.ENTER))
+        else if (event.getCode().equals(KeyCode.ENTER))
         {
             tempButton = buttons.get("enter");
             tempButton.arm();
             tempButton.setStyle("-fx-background-color: blue");
         }
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
-        // TODO
-        for(Node tempNode : gpKeyboard.getChildren())
+        else if (event.getCode().equals(KeyCode.BACK_SPACE))
         {
-            if(tempNode instanceof Button)
-            {
-                Button tempButton = (Button)tempNode;
-                buttons.put(tempButton.getText().toLowerCase(), tempButton);
-            }          
+            tempButton = buttons.get("backspace");
+            tempButton.arm();
+            tempButton.setStyle("-fx-background-color: blue");
         }
-    }       
+    }
+
+    private void showWelcomeScreen()
+    {
+        apWelcomeScreen.setVisible(true);
+        gpKeyboard.setVisible(true);
+        isWelcomeScreenShowingi = true;
+
+        gpNumberPad.setVisible(false);
+        isDepositScreenShowing = false;
+        isWithdrawalScreenShowing = false;
+        isSetupAccountScreenShowing = false;
+
+        btnLeftOne.setOnAction((event) ->
+        {
+            showSetupAccountScreen();
+        });
+        btnLeftTwo.setOnAction(null);
+        btnLeftThree.setOnAction(null);
+        btnLeftFour.setOnAction(null);
+
+        btnRightOne.setOnAction((event) ->
+        {
+            showWithdrawalScreen();
+        });
+        btnRightTwo.setOnAction((event) ->
+        {
+            showDepositScreen();
+        });
+        btnRightThree.setOnAction(null);
+        btnRightFour.setOnAction(null);
+    }
+
+    private void showSetupAccountScreen()
+    {
+//        apWelcomeScreen.setVisible(true);
+//        gpKeyboard.setVisible(true);
+//        isWelcomeScreenShowingi = true;
+//
+//        gpNumberPad.setVisible(false);
+//        isDepositScreenShowing = false;
+//        isWithdrawalScreenShowing = false;
+//        isSetupAccountScreenShowing = false;
+//
+//        btnLeftOne.setOnAction((event)->{showSetupAccountScreen();});
+//        btnLeftTwo.setOnAction(null);
+//        btnLeftThree.setOnAction(null);
+//        btnLeftFour.setOnAction(null);
+//
+//        btnRightOne.setOnAction((event)->{showWithdrawalScreen();});
+//        btnRightTwo.setOnAction((event)->{showDepositScreen();});
+//        btnRightThree.setOnAction(null);
+//        btnRightFour.setOnAction(null);
+    }
+
+    private void showDepositScreen()
+    {
+//        apWelcomeScreen.setVisible(true);
+//        gpKeyboard.setVisible(true);
+//        isWelcomeScreenShowingi = true;
+//
+//        gpNumberPad.setVisible(false);
+//        isDepositScreenShowing = false;
+//        isWithdrawalScreenShowing = false;
+//        isSetupAccountScreenShowing = false;
+//
+//        btnLeftOne.setOnAction((event)->{showSetupAccountScreen();});
+//        btnLeftTwo.setOnAction(null);
+//        btnLeftThree.setOnAction(null);
+//        btnLeftFour.setOnAction(null);
+//
+//        btnRightOne.setOnAction((event)->{showWithdrawalScreen();});
+//        btnRightTwo.setOnAction((event)->{showDepositScreen();});
+//        btnRightThree.setOnAction(null);
+//        btnRightFour.setOnAction(null);
+    }
+
+    private void showWithdrawalScreen()
+    {
+//        apWelcomeScreen.setVisible(true);
+//        gpKeyboard.setVisible(true);
+//        isWelcomeScreenShowingi = true;
+//
+//        gpNumberPad.setVisible(false);
+//        isDepositScreenShowing = false;
+//        isWithdrawalScreenShowing = false;
+//        isSetupAccountScreenShowing = false;
+//
+//        btnLeftOne.setOnAction((event)->{showSetupAccountScreen();});
+//        btnLeftTwo.setOnAction(null);
+//        btnLeftThree.setOnAction(null);
+//        btnLeftFour.setOnAction(null);
+//
+//        btnRightOne.setOnAction((event)->{showWithdrawalScreen();});
+//        btnRightTwo.setOnAction((event)->{showDepositScreen();});
+//        btnRightThree.setOnAction(null);
+//        btnRightFour.setOnAction(null);
+    }
 }
